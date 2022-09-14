4d40ff6b3ab10fc1b146625e0b5f229835d0c46b
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-09-14 22:40:50 +0300

    start servers from python

---
+++ linux_ubuntu/SERVER_MAIN.py
@@ -0,0 +1,92 @@
+'''
+author: Egor Bakay <egor_bakay@inbox.ru>
+write:  august 2022
+modify: september 2022
+'''
+
+import subprocess # cmd
+from threading import Thread
+from time import time
+import os
+import os.path
+from sys import platform
+from time import sleep
+
+#====================================================================================================================================================================================
+#================================================================================= CONFIG ===========================================================================================
+#====================================================================================================================================================================================
+
+very_fast_crush = 5 # если проект крашится быстрее чем раз в n секунд, то он будет автоматически отключен
+sleep_after_restart = 5 # n секунд программа спит, перед рестартом проекта
+run_command = "python3 "
+
+'''
+path = ["D:/SERVER/cache/test.py",
+        "D:/SERVER/operation/SuCCess/TelBotMain.py",
+        "D:/SERVER/operation/box_distributor/TelBotMain.py",
+        "D:/SERVER/cache/test.py"]
+'''
+#====================================================================================================================================================================================
+#================================================================================= AUTO SET =========================================================================================
+#====================================================================================================================================================================================
+
+if not platform.upper().find("LINUX")>-1: 
+    print("ERROR: this is not LINUX OC")
+    sleep(10)
+    exit()
+
+from os.path import abspath
+config_path = abspath(__file__).replace(__file__, "config.txt")
+print(config_path)
+
+if not os.path.isfile(config_path):
+    print("ERROR: not <config.txt> in catalog")
+    exit()
+file = open(config_path, "r")
+path = file.readlines()
+print(path)
+for i in range(0,len(path)):
+    if path[i][-1]=='\n':
+        path[i] = path[i][0:-1]
+#print(path)
+file.close()
+
+#====================================================================================================================================================================================
+#================================================================================= MAIN =============================================================================================
+#====================================================================================================================================================================================
+
+class StartServer(Thread):
+    
+    def __init__(self,python_path): 
+        Thread.__init__(self)
+        self.python_path = run_command + python_path
+        self.python_path_folder = python_path
+        i = len(self.python_path_folder)-1
+        while self.python_path_folder[i]!='/' and self.python_path_folder[i]!=chr(92):
+            i-=1
+        self.python_main = run_command + self.python_path_folder[(i+1):]
+        self.python_path_folder = self.python_path_folder[:i]
+        self.timer = 0
+    
+    def run(self): 
+        print("START: " + self.python_path)
+        while time()-self.timer>very_fast_crush: # чтобы если крашится чаще чем каждые 5 секунд, выключить это
+            if self.timer!=0:
+                print("ERROR and restart: " + self.python_path)
+                sleep(sleep_after_restart)
+            self.timer = time()
+            #subprocess.run(self.python_path)
+            subprocess.run(self.python_main,cwd=self.python_path_folder)
+            
+        print("FATAL ERROR: " + self.python_path)
+
+# file = open(str(os.getcwd())+"D:/SERVER/config.txt", "r")
+# path = file.read().split('\n')
+# file.close()
+
+threads = []
+for a in path:
+    threads.append(StartServer(a))
+    threads[-1].start()
+
+print("ALL SERVERS STARTED")
